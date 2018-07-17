e22b37fb67c34683c0bd5a9c8887babc5a56245b
Lewin Chan <lewin.chan@adaptris.com>
2018-07-17 09:07:31 +0100

    INTERLOK-2315 Update to 2.10.5
    - Add pretty UML.
    - Add note about how this is for ehcache 2.x

--- interlok-ehcache/src/main/java/com/adaptris/core/cache/ehcache/package-info.java
+++ interlok-ehcache/src/main/java/com/adaptris/core/cache/ehcache/package-info.java
@@ -1,4 +1,12 @@
 /**
+ * <p>
  * {@link com.adaptris.core.cache.Cache} implementations based on ehcache.
+ * </p>
+ * <p>
+ * This is package is designed with with with ehcache 2.x ({@code net.sf.ehcache:ehcache:2.10.x}). If you wish to use ehcache3
+ * ({@code org.ehcache:ehcache:3.x}) then you should use the {@code interlok-jsr107-cache} artefact instead.
+ * </p>
+ * <img alt="UML" src="package.svg"/>
+ * 
  */
 package com.adaptris.core.cache.ehcache;
